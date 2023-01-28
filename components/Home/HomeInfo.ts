c942c2245160f019a319a9df56470eaff1477404
Vladimir Timotijevic <timotijevicvlada@gmail.com>
2023-01-28 16:15:06 +0100

    Error solved

--- components/Home/HomeInfo.ts
+++ components/Home/HomeInfo.ts
@@ -2,41 +2,21 @@ export const appDescription = [
     {
         subject: "Quiz app",
         route: "/quiz",
-        description: `On this application you can choose your name, the number of questions,
-        the category and the difficulty of the questions you want. During the quiz you have 20 seconds to answer each question.
-        When selecting a question, the selected question will show red or green color if you have answered correctly.
-        At the bottom you have a progress bar that tells you how many questions you have completed in the quiz.
-        At the end of the quiz, you will receive information about the total number of points you have won.`
+        description: "On this application you can choose your name, the number of questions, the category and the difficulty of the questions you want. During the quiz you have 20 seconds to answer each question. When selecting a question, the selected question will show red or green color if you have answered correctly. At the bottom you have a progress bar that tells you how many questions you have completed in the quiz. At the end of the quiz, you will receive information about the total number of points you have won."
     },
     {
         subject: "Todo app",
         route: "/todo-list",
-        description: ` On this application, you can create your todo task by clicking on the plus icon,
-        where a data entry window will open. You can later mark each created task as done,
-        edit it, or archive it. In the sidebar, you have filters by which you can select specific tasks
-        according to a certain criteria. If you archived a task, you can revert it back or
-        delete it permanently.`
+        description: "On this application, you can create your todo task by clicking on the plus icon, where a data entry window will open. You can later mark each created task as done, edit it, or archive it. In the sidebar, you have filters by which you can select specific tasks according to a certain criteria. If you archived a task, you can revert it back or delete it permanently."
     },
     {
         subject: "Budget app",
         route: "/budget",
-        description: `This application is for controlling your budget.
-        You can add your income and expenses by entering information in the inputs.
-        When you enter a certain value, it will be projected into the transaction history,
-        where you can delete it if you wish. In the sidebar you have information about the
-        current amount of your budget, about expenses and income. You also have a graphical
-        representation of expenses and income.`
+        description: "This application is for controlling your budget. You can add your income and expenses by entering information in the inputs. When you enter a certain value, it will be projected into the transaction history, where you can delete it if you wish. In the sidebar you have information about the current amount of your budget, about expenses and income. You also have a graphical representation of expenses and income."
     },
     {
         subject: "Rick and Morty TV series",
         route: "/caracters",
-        description: `This app pulls information from API.
-        When you load the app it will give you the first 20 characters,
-        if you scroll down it will load new characters from the same api
-        and you will have infinite scroll as long as there is data. You can also click on each
-        individual character and see details about it on a separate page. You can search
-        characters using the input or using the filters offered to you in the form of a dropdown list.
-        You can also notice data loading states, as well as errors if there are no required
-        characters according to your criteria.`
+        description: "This app pulls information from API. When you load the app it will give you the first 20 characters, if you scroll down it will load new characters from the same api and you will have infinite scroll as long as there is data. You can also click on each individual character and see details about it on a separate page. You can search characters using the input or using the filters offered to you in the form of a dropdown list. You can also notice data loading states, as well as errors if there are no required characters according to your criteria."
     },
 ]
