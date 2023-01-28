ccca3204b3d1e5adb10b6608a03e462d84333418
Vladimir Timotijevic <timotijevicvlada@gmail.com>
2023-01-28 15:01:50 +0100

    Home page added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import css from "./index.module.scss";
 
 //components
 import Home from 'components/Home/Home';
@@ -5,6 +6,8 @@ import Home from 'components/Home/Home';
 export default function Index() {
 
   return (
+    <div className={css.container}>
       <Home />
+    </div>
   )
 }
